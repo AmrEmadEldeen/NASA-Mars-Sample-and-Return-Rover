91f51bd1ef8388d9b4cf2c69d6ba5b057828a598
MoFarrag <119762951+MoFarrag@users.noreply.github.com>
2023-01-03 22:06:09 +0200

    Update perception.py

--- code/perception.py
+++ code/perception.py
@@ -16,7 +16,8 @@ def color_thresh(img, rgb_thresh=(160, 160, 160)):
     color_select[above_thresh] = 1
     # Return the binary image
     return color_select
-# To detect rocks, applies a threshold above and below
+
+# This is used to detect rocks, applies a threshold above and below
 def color_squeeze(img, rgb_d, rgb_u):
     # Create an empty array the same size in x and y as the image                                                                                                               
     # but just a single channel                                                                                                                                                 
@@ -68,7 +69,6 @@ def translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale):
 
 
 # Define a function to apply rotation and translation (and clipping)
-# Once you define the two functions above this function should work
 def pix_to_world(xpix, ypix, xpos, ypos, yaw, world_size, scale):
     # Apply rotation
     xpix_rot, ypix_rot = rotate_pix(xpix, ypix, yaw)
@@ -81,39 +81,39 @@ def pix_to_world(xpix, ypix, xpos, ypos, yaw, world_size, scale):
     return x_pix_world, y_pix_world
 
 # Define a function to perform a perspective transform
-def perspect_transform(img, src, dst):
-           
+def perspect_transform(img, src, dst):           
     M = cv2.getPerspectiveTransform(src, dst)
     warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))# keep same size as input image
     mask = cv2.warpPerspective(np.ones_like(img[:,:,0]), M, (img.shape[1], img.shape[0]))
+    
     return warped, mask
 
 
-#A function to cut off robot vision by range, 5m
+# Define a function to cut off robot vision by range, default is 5m
 def range_limit(xpix, ypix, range=50):
     dist = np.sqrt(xpix**2 + ypix**2)
     return xpix[dist < range], ypix[dist < range]
 
-
-
-# Apply the above functions in succession and update the Rover state accordingly
 def perception_step(Rover):
     # Perform perception steps to update Rover()
-    # NOTE: camera image is coming to you in Rover.img
-         
+    
+    # NOTE: camera image is coming in Rover.img
+
+    img = Rover.img
+    
     # 1) Define source and destination points for perspective transform
     dst_size = 5
     bottom_offset = 6
     source = np.float32([[14, 140], [301 ,140],[200, 96], [118, 96]])
-    destination = np.float32([[Rover.img.shape[1]/2 - dst_size, Rover.img.shape[0] - bottom_offset],
-                  [Rover.img.shape[1]/2 + dst_size, Rover.img.shape[0] - bottom_offset],
-                  [Rover.img.shape[1]/2 + dst_size, Rover.img.shape[0] - 2*dst_size - bottom_offset], 
-                  [Rover.img.shape[1]/2 - dst_size, Rover.img.shape[0] - 2*dst_size - bottom_offset],
+    destination = np.float32([[img.shape[1]/2 - dst_size, img.shape[0] - bottom_offset],
+                  [img.shape[1]/2 + dst_size, img.shape[0] - bottom_offset],
+                  [img.shape[1]/2 + dst_size, img.shape[0] - 2*dst_size - bottom_offset], 
+                  [img.shape[1]/2 - dst_size, img.shape[0] - 2*dst_size - bottom_offset],
                   ])
-        
+
     # 2) Apply perspective transform
-    warped, mask = perspect_transform(Rover.img, source, destination)
-    
+    warped, mask = perspect_transform(img, source, destination)
+
     # 3) Apply color threshold to identify navigable terrain/obstacles/rock samples
     red_down = 120
     green_down = 110
@@ -130,41 +130,36 @@ def perception_step(Rover):
     
     obs_map = np.absolute(np.float32(threshed)-1) * mask
     
+    # 4) Update Rover.vision image displayed on left side of screen
+    Rover.vision_image[:,:,2] = threshed * 255
+    Rover.vision_image[:,:,0] = obs_map * 255
     
-    # 4) Update Rover.vision_image (this will be displayed on left side of screen)
-
-    Rover.vision_image[:,:,0] = obs_map*255
-    Rover.vision_image[:,:,2] = threshed *255
-   
-       
-
-    # 5) Convert map image pixel values to rover-centric coords
-    
+    # 5) Convert thresholded image pixel values to rover-centric coords and cut off 5m
     xpix, ypix = rover_coords(threshed)
     xobs, yobs = rover_coords(obs_map)
+
     Rover.nav_x = xpix
     Rover.nav_y = ypix
     
-    
-    # 6) Convert rover-centric pixel values to world coordinates
-       scale = 2*dst_size
+    xpix, ypix = range_limit(xpix, ypix)
+    xobs, yobs = range_limit(xobs, yobs)
+
+    # 6) Convert rover-centric pixel values to world coords
+    scale = 2*dst_size
     world_size = Rover.worldmap.shape[0]
     
     navigable_x_world, navigable_y_world = pix_to_world(xpix, ypix, Rover.pos[0], Rover.pos[1], Rover.yaw, world_size, scale)
     obs_x_world, obs_y_world = pix_to_world(xobs, yobs, Rover.pos[0], Rover.pos[1], Rover.yaw, world_size, scale)
     
-    # 7) Update Rover worldmap (to be displayed on right side of screen)
-    
-  
+    # 7) Update worldmap if pitch and roll are small (to be displayed on right side of screen)
     if np.absolute(Rover.pitch) < 0.7 or np.absolute(Rover.pitch - 360) < 0.7:
         if np.absolute(Rover.roll) < 0.7 or np.absolute(Rover.roll - 360) < 0.7:
             Rover.worldmap[navigable_y_world, navigable_x_world, 2] += 10
             Rover.worldmap[obs_y_world, obs_x_world, 0] += 1
             free_points = Rover.worldmap[:,:,2] > 0
             Rover.worldmap[free_points, 0] = 0
-    
-     
-  # 8) Convert rover-centric pixels to polar coordinates
+
+    # 8) Convert rover-centric pixels to polar coordinates
     dist, angles = to_polar_coords(xpix, ypix)
     Rover.nav_angles = angles
 
@@ -186,7 +181,5 @@ def perception_step(Rover):
         Rover.vision_image[:,:,1] = 255 * squeezed
     else:
         Rover.vision_image[:,:,1] = 0
- 
-    
     
     return Rover
