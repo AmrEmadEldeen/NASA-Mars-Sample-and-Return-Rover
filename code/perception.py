729d518d9b66ad98197485cd0d2f96ae4781012d
MoFarrag <119762951+MoFarrag@users.noreply.github.com>
2023-01-03 14:37:18 +0200

    Update perception.py

--- code/perception.py
+++ code/perception.py
@@ -87,10 +87,14 @@ def perspect_transform(img, src, dst):
     warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))# keep same size as input image
     mask = cv2.warpPerspective(np.ones_like(img[:,:,0]), M, (img.shape[1], img.shape[0]))
     return warped, mask
+
+
 #A function to cut off robot vision by range, 5m
 def range_limit(xpix, ypix, range=50):
     dist = np.sqrt(xpix**2 + ypix**2)
     return xpix[dist < range], ypix[dist < range]
+
+
 
 # Apply the above functions in succession and update the Rover state accordingly
 def perception_step(Rover):
